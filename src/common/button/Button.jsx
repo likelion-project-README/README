1771240a0637134fa88057e9713f9bc7ef7f992a
Jiyeon Ko <goprawgo@gmail.com>
2022-12-14 14:24:51 +0900

    💄 Feat : Button 컴포넌트 추가

---
+++ src/common/button/Button.jsx
@@ -0,0 +1,24 @@
+import Btn from './Button.Style';
+
+/*
+ * size     - lg, md, sm, xs (피그마 참고)
+ *          - 작성하지 않을 경우 default 사이즈로 들어갑니다
+ *
+ * tit      - 버튼 내부에 들어갈 텍스트를 자유롭게 작성하시면 됩니다
+ *
+ * state    - active, disabled (배경색을 의미합니다)
+ *          - 작성하지 않을 경우 기본값인 흰색으로 들어갑니다
+ *
+ * txtcolor - black
+ *          - 작성하지 않을 경우 기본값인 흰색으로 들어갑니다
+ */
+
+const Button = ({ size, tit, state, txtcolor }) => {
+  return (
+    <Btn type='button' state={state} size={size} txtcolor={txtcolor}>
+      <span>{tit}</span>
+    </Btn>
+  );
+};
+
+export default Button;
