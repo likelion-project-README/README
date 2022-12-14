4c1f165713376286641fc841f927577f788e5592
Cheorizzang <ouov2gg@gmail.com>
2022-12-14 21:39:41 +0900

    :lipstick:Feat : 404페이지 UI 구현

---
+++ src/pages/page404/Page404.Style.jsx
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+import logo from '../../assets/logo.svg';
+
+export const PageWrap = styled.section`
+  width: 390px;
+  height: 820px;
+  text-align: center;
+`;
+
+export const PageTit = styled.h2`
+  /* ir */
+  position: absolute;
+  left: -10000px;
+  top: auto;
+  width: 1px;
+  height: 1px;
+  overflow: hidden;
+`;
+
+export const LogoImg = styled.div`
+  display: inline-block;
+  background: url(${logo});
+  width: 196px;
+  height: 171px;
+  margin: 212px 0 15px 0;
+`;
+
+export const Message = styled.p`
+  font-size: 14px;
+  color: var(--sub-text-color);
+  margin-bottom: 36px;
+`;
---
+++ src/pages/page404/Page404.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Button from '../../common/button/Button';
+import * as S from './Page404.Style';
+
+const Page404 = () => {
+  return (
+    <S.PageWrap>
+      <S.PageTit>404 페이지</S.PageTit>
+      <S.LogoImg />
+      <S.Message>페이지를 찾을 수 없습니다. :(</S.Message>
+      <Button tit='이전 페이지' state='active' />
+    </S.PageWrap>
+  );
+};
+
+export default Page404;
