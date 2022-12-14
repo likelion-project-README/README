250627c9d398020267ac76c13b61ef5b8b1b07b8
Cheorizzang <ouov2gg@gmail.com>
2022-12-14 14:48:50 +0900

    🎨Modify : ProfileEdit 페이지 InputBox 컴포넌트로 변경

--- src/pages/profileEdit/ProfileEdit.jsx
+++ src/pages/profileEdit/ProfileEdit.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import TopBanner from '../../common/topBanner/TopBanner';
+import InputBox from '../../common/inputBox/InputBox';
 import * as S from './ProfileEdit.Style';
 
 const ProfileEdit = () => {
@@ -15,29 +16,21 @@ const ProfileEdit = () => {
           <S.ImgUploadLab htmlFor='userImg' />
           <S.ImgUploadInp type='file' id='userImg' />
         </S.ImgWrap>
-        <S.InputBox>
-          <S.InputBoxTit htmlFor='userName'>사용자 이름</S.InputBoxTit>
-          <S.InputBoxInp
-            placeholder='2~10자 이내여야 합니다.'
-            id='userName'
-            minLength='2'
-            maxLength='10'
-          />
-        </S.InputBox>
-        <S.InputBox>
-          <S.InputBoxTit htmlFor='userID'>계정 ID</S.InputBoxTit>
-          <S.InputBoxInp
-            placeholder='영문, 숫자, 특수문자(.),(_)만 사용 가능합니다.'
-            id='userID'
-          />
-        </S.InputBox>
-        <S.InputBox>
-          <S.InputBoxTit htmlFor='userIntro'>소개</S.InputBoxTit>
-          <S.InputBoxInp
-            placeholder='자신과 판매할 상품에 대해 소개해 주세요!'
-            id='userIntro'
-          />
-        </S.InputBox>
+        <InputBox
+          label='사용자 이름'
+          id='userName'
+          placeholder='2~10자 이내여야 합니다.'
+        />
+        <InputBox
+          label='계정 ID'
+          id='userID'
+          placeholder='영문, 숫자, 특수문자(.),(_)만 사용 가능합니다.'
+        />
+        <InputBox
+          label='소개'
+          id='userIntro'
+          placeholder='자신과 판매할 상품에 대해 소개해 주세요!'
+        />
       </form>
     </S.ProfileEditWrap>
   );
