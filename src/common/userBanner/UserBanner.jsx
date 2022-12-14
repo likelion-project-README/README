64d77d2ac21093e7dfe3808f15ab3af07a171508
heejiyang <dianoheeji@naver.com>
2022-12-14 18:36:48 +0900

    🎨Modify : UserBanner button 적용 수정

--- src/common/userBanner/UserBanner.jsx
+++ src/common/userBanner/UserBanner.jsx
@@ -1,17 +1,39 @@
 import React from 'react';
+import Button from '../button/Button';
 import * as S from './UserBanner.Style';
 
-const UserBanner = () => {
-  return (
-    <S.UserBannerWrapper>
-      <S.UserImg />
-      <S.UserInfo>
-        <S.UserNickName>애월읍 위니브 감귤농장</S.UserNickName>
-        <S.UserId>@ weniv_Mandarin</S.UserId>
-      </S.UserInfo>
-      <S.FollowBtn>팔로우</S.FollowBtn>
-    </S.UserBannerWrapper>
-  );
+/*
+UserBanner 사용 명세
+
+UserBanner(state)
+state - active, disabled
+*/
+
+const UserBanner = ({ state }) => {
+  if (state === 'btn-disabled') {
+    return (
+      <S.UserBannerWrapper>
+        <S.UserImg />
+        <S.UserInfo>
+          <S.UserNickName>애월읍 위니브 감귤농장</S.UserNickName>
+          <S.UserId>@ weniv_Mandarin</S.UserId>
+        </S.UserInfo>
+      </S.UserBannerWrapper>
+    );
+  }
+  if (state === 'btn-active') {
+    return (
+      <S.UserBannerWrapper>
+        <S.UserImg />
+        <S.UserInfo>
+          <S.UserNickName>애월읍 위니브 감귤농장</S.UserNickName>
+          <S.UserId>@ weniv_Mandarin</S.UserId>
+        </S.UserInfo>
+        <Button state='active' size='xs' tit='팔로우' />
+      </S.UserBannerWrapper>
+    );
+  }
+  return <S.UserBannerWrapper>state를 전달하세요.</S.UserBannerWrapper>;
 };
 
 export default UserBanner;
