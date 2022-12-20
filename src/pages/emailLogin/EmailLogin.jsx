86f8ce44279a44f13819796cef360bd2b5adbbf5
heejiyang <dianoheeji@naver.com>
2022-12-20 22:01:34 +0900

    ✨Feat : emailLogin 기능 구현

--- src/pages/emailLogin/EmailLogin.jsx
+++ src/pages/emailLogin/EmailLogin.jsx
@@ -1,21 +1,108 @@
-import React from 'react';
-import InputBox from '../../common/inputBox/InputBox';
-import Button from '../../common/button/Button';
+import React, { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import * as S from './EmailLogin.Style';
 
+import EmailLoginAPI from '../../api/emailLoginAPI';
+import InputBox from '../../common/inputBox/InputBox';
+import Button from '../../common/button/Button';
+
 const EmailLoginPage = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+  const navigate = useNavigate();
+  const [btnActive, setBtnActive] = useState('');
+  // const [loginValue, setLoginValue] = useState({
+  //   email: '',
+  //   password: '',
+  // });
+
+  // 버튼 활성화
+  useEffect(() => {
+    if (email && password) {
+      setBtnActive(true);
+    } else {
+      setBtnActive(false);
+    }
+  }, [email, password]);
+
+  const handleClick = () => {
+    navigate('/signUp/profileSetting', {
+      state: {
+        email,
+        password,
+      },
+    });
+  };
+
+  // validation
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    EmailLoginAPI({
+      user: {
+        email,
+        password,
+      },
+    })
+      .then((data) => {
+        const emaiReg =
+          /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+        if (!emaiReg.test(email)) {
+          setEmailError('올바른 이메일 형식이 아닙니다.');
+        } else if (data.statue === 422) {
+          setPasswordError(data.message);
+        } else {
+          localStorage.setItem('token', data.user.token);
+          const userData = data.user;
+          console.log(data);
+          navigate('/home');
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <S.LoginSec>
       <S.LoginTit>로그인</S.LoginTit>
-      <S.EmailLoginForm>
-        <InputBox label='이메일' id='이메일' />
-        <S.Password>
-          <S.PasswordLab htmlFor='passwordInput'>비밀번호</S.PasswordLab>
-          <S.PasswordInp id='passwordInput' type='password' />
-        </S.Password>
+      <S.EmailLoginForm onSubmit={handleSubmit}>
+        <InputBox
+          label='이메일'
+          id='email'
+          type='email'
+          // required
+          // value={email}
+          setInput={setEmail}
+          placeholder='이메일 주소를 입력해주세요'
+          message={emailError}
+        />
+        <InputBox
+          label='비밀번호'
+          id='password'
+          type='password'
+          // required
+          // value={password}
+          setInput={setPassword}
+          minLength='6'
+          placeholder='비밀번호를 입력해주세요'
+        />
       </S.EmailLoginForm>
-      <Button tit='로그인' state='active' size='lg' txtcolor='#fff'></Button>
-      <S.SignUpTxt type='button'>이메일로 회원가입</S.SignUpTxt>
+      <Button
+        type='submit'
+        size='lg'
+        tit='로그인'
+        onClick={handleClick}
+        isActive={btnActive}
+        disabled={password.length < 6 ? 'disabled' : null}
+        message={
+          password.length < 6 ? '비밀번호는 6자리 이상이어야 합니다' : null
+        }
+      />
+      <S.SignUpTxt type='button'>
+        <Link to='/signUp'>이메일로 회원가입</Link>
+      </S.SignUpTxt>
     </S.LoginSec>
   );
 };
--- src/pages/join/Join.jsx
+++ src/pages/join/Join.jsx
@@ -1,20 +1,64 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import InputBox from '../../common/inputBox/InputBox';
 import Button from '../../common/button/Button';
 import * as S from './Join.Style';
 
 const JoinPage = () => {
+  const [btnActive, setBtnActive] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (email && password) {
+      setBtnActive(true);
+    } else {
+      setBtnActive(false);
+    }
+  }, [email, password]);
+
+  const handleClick = () => {
+    navigate('/signUp/profileSetting', {
+      state: {
+        email,
+        password,
+      },
+    });
+  };
   return (
     <S.JoinSec>
       <S.JoinTit>이메일로 회원가입</S.JoinTit>
       <S.JoinForm>
-        <InputBox label='이메일' id='이메일' />
-        <S.Password>
-          <S.PasswordLab htmlFor='passwordInput'>비밀번호</S.PasswordLab>
-          <S.PasswordInp id='passwordInput' type='password' />
-        </S.Password>
+        <InputBox
+          label='이메일'
+          id='이메일'
+          type='email'
+          required
+          value={email}
+          setInput={setEmail}
+          // onChange={handleData}
+          placeholder='이메일 주소를 입력해주세요'
+        />
+        <InputBox
+          label='비밀번호'
+          id='비밀번호'
+          type='password'
+          required
+          value={password}
+          setInput={setPassword}
+          // onChange={handleData}
+          placeholder='비밀번호를 입력해주세요'
+        />
       </S.JoinForm>
-      <Button tit='다음' state='active' size='lg' txtcolor='#fff'></Button>
+      <Button
+        type='submit'
+        size='lg'
+        tit='다음'
+        onClick={handleClick}
+        isActive={btnActive}
+        // isActive={email.length !== 0 && password.length !== 0 ? true : 'false'}
+      ></Button>
     </S.JoinSec>
   );
 };
