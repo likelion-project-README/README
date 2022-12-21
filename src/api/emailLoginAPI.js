065113c3a58decf36e81ce02a4a003dcc1c798c5
heejiyang <dianoheeji@naver.com>
2022-12-21 09:40:48 +0900

    ✨Add : validation 기능  추가

--- src/api/emailLoginAPI.js
+++ src/api/emailLoginAPI.js
@@ -1,50 +1,47 @@
-const url = 'https://mandarin.api.weniv.co.kr';
-
-const EmailLoginAPI = {
-  async getLogin(email, password) {
-    const res = await fetch(`${url}/user/login`, {
+const getEmailLoginAPI = async (event) => {
+  event.preventDefault();
+  try {
+    const res = await fetch('https://mandarin.api.weniv.co.kr/user/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({
-        user: {
-          email: email,
-          password: password,
-        },
-      }),
+      body: JSON.stringify(loginData),
     });
     const json = await res.json();
     console.log(json);
 
-    const token = data.user('token');
-    localStorage.setItem('token', token);
-    return data;
-  },
-
-  async getJoin(userName, email, password, id, introduce, img) {
-    const userData = {
-      user: {
-        username: userName,
-        email: email,
-        password: password,
-        accountname: id,
-        intro: introduce,
-        image: img,
-      },
-    };
-
-    const res = await fetch(`${url}/user`, {
-      method: 'POST',
-      headers: {
-        'Content-type': 'application/json',
-      },
-      body: JSON.stringify(userData),
-    });
-
-    const json = await res.json();
-    return json;
-  },
+    // const token = data.user('token');
+    // localStorage.setItem('token', token);
+    // return data;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
 };
 
-export default EmailLoginAPI;
+export default getEmailLoginAPI;
+//   async getJoin(userName, email, password, id, introduce, img) {
+//     const userData = {
+//       user: {
+//         username: userName,
+//         email: email,
+//         password: password,
+//         accountname: id,
+//         intro: introduce,
+//         image: img,
+//       },
+//     };
+
+//     const res = await fetch('https://mandarin.api.weniv.co.kr/user', {
+//       method: 'POST',
+//       headers: {
+//         'Content-type': 'application/json',
+//       },
+//       body: JSON.stringify(userData),
+//     });
+
+//     const json = await res.json();
+//     return json;
+//   },
+// };
--- src/pages/emailLogin/EmailLogin.jsx
+++ src/pages/emailLogin/EmailLogin.jsx
@@ -1,31 +1,37 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import * as S from './EmailLogin.Style';
 
-import EmailLoginAPI from '../../api/emailLoginAPI';
 import InputBox from '../../common/inputBox/InputBox';
 import Button from '../../common/button/Button';
+// import getEmailLoginAPI from '../../api/emailLoginAPI';
+
+// const loginData = {
+//   user: {
+//     email: emailRef.current.value,
+//     password: passwordRef.current.value,
+//   },
+// };
 
 const EmailLoginPage = () => {
   const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
   const [emailError, setEmailError] = useState('');
+  const [emailValid, setEmailValid] = useState('');
   const [passwordError, setPasswordError] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordValid, setPasswordValid] = useState('');
+  const [btnActive, setBtnActive] = useState(true);
   const navigate = useNavigate();
-  const [btnActive, setBtnActive] = useState('');
-  // const [loginValue, setLoginValue] = useState({
-  //   email: '',
-  //   password: '',
-  // });
 
-  // 버튼 활성화
-  useEffect(() => {
-    if (email && password) {
-      setBtnActive(true);
-    } else {
-      setBtnActive(false);
-    }
-  }, [email, password]);
+  const emailRef = useRef('');
+  const passwordRef = useRef('');
+
+  const loginData = {
+    user: {
+      email: emailRef.current.value,
+      password: passwordRef.current.value,
+    },
+  };
 
   const handleClick = () => {
     navigate('/signUp/profileSetting', {
@@ -36,38 +42,83 @@ const EmailLoginPage = () => {
     });
   };
 
-  // validation
-  const handleSubmit = (event) => {
+  // 이메일 유효성 검사
+  const emailInputCheck = ({ target }) => {
+    const emailInputValue = target.value;
+    const checkEmail =
+      /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+    if (emailInputCheck === '') {
+      setEmailError('입력해주세요.');
+      setEmailValid(false);
+    } else if (!checkEmail.test(emailInputValue)) {
+      setEmailError('올바른 이메일 형식이 아닙니다.');
+      setEmailValid(false);
+    } else {
+      setEmailError('');
+      setEmailValid(true);
+      // localStorage.setItem('token', data.user.token);
+      // const userData = data.user;
+      // console.log(data);
+      // navigate('/home');
+    }
+  };
+  // 비밀번호 유효성 검사
+  const passwordInputCheck = ({ target }) => {
+    const passwordInputValue = target.value;
+    if (passwordInputCheck === '') {
+      setPasswordError('입력해주세요.');
+      setPasswordValid(false);
+    } else if (!passwordInputValue.length < 6) {
+      setPasswordError('비밀번호는 6자 이상이어야 합니다.');
+      setPasswordValid(false);
+    } else {
+      setPasswordError('');
+      setPasswordValid(true);
+    }
+  };
+
+  const goProfileSetting = () => {
+    return emailValid && passwordValid
+      ? setBtnActive(false)
+      : setBtnActive(true);
+  };
+  // 버튼 활성화
+  // useEffect(() => {
+  //   emailValid && passwordValid ? setBtnActive(false) : setBtnActive(true);
+  // if (email && password) {
+  //   setBtnActive(true);
+  // } else {
+  //   setBtnActive(false);
+  // }
+  // , [email, password]
+  // });
+
+  // getEmailLoginAPI api폴더 내의 내용
+  const getEmailLoginAPI = async (event) => {
     event.preventDefault();
-    EmailLoginAPI({
-      user: {
-        email,
-        password,
-      },
-    })
-      .then((data) => {
-        const emaiReg =
-          /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
-        if (!emaiReg.test(email)) {
-          setEmailError('올바른 이메일 형식이 아닙니다.');
-        } else if (data.statue === 422) {
-          setPasswordError(data.message);
-        } else {
-          localStorage.setItem('token', data.user.token);
-          const userData = data.user;
-          console.log(data);
-          navigate('/home');
-        }
-      })
-      .catch((error) => {
-        console.log(error);
+    try {
+      const res = await fetch('https://mandarin.api.weniv.co.kr/user/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(loginData),
       });
+      const json = await res.json();
+      console.log(json);
+      // const token = data.user('token');
+      // localStorage.setItem('token', token);
+      // return data;
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
   };
 
   return (
     <S.LoginSec>
       <S.LoginTit>로그인</S.LoginTit>
-      <S.EmailLoginForm onSubmit={handleSubmit}>
+      <S.EmailLoginForm onSubmit={getEmailLoginAPI}>
         <InputBox
           label='이메일'
           id='email'
@@ -77,6 +128,7 @@ const EmailLoginPage = () => {
           setInput={setEmail}
           placeholder='이메일 주소를 입력해주세요'
           message={emailError}
+          onKeyUp={goProfileSetting}
         />
         <InputBox
           label='비밀번호'
@@ -96,9 +148,7 @@ const EmailLoginPage = () => {
         onClick={handleClick}
         isActive={btnActive}
         disabled={password.length < 6 ? 'disabled' : null}
-        message={
-          password.length < 6 ? '비밀번호는 6자리 이상이어야 합니다' : null
-        }
+        message={passwordError}
       />
       <S.SignUpTxt type='button'>
         <Link to='/signUp'>이메일로 회원가입</Link>
