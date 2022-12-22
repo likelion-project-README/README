f2a24f1389b7b017c835db468ebc9f5d8f27db15
Jiyeon Ko <goprawgo@gmail.com>
2022-12-22 16:25:53 +0900

    ✨Add : 검색어 입력 시 매칭된 단어 강조표시 기능 구현

--- src/pages/search/Search.Style.jsx
+++ src/pages/search/Search.Style.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 // 주석처리된 코드는 추후 반응형 작업 시 수정 예정입니다
 
@@ -26,6 +27,50 @@ export const SearchedUserUl = styled.ul`
   height: 712px;
   padding: 20px 16px;
   overflow-y: scroll;
+`;
+
+export const SearchedUser = styled.div`
+  max-width: 358px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const UserImg = styled.img`
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  background-color: #dbdbdb;
+`;
+
+export const UserInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 5px 0;
+  margin-left: 12px;
+  margin-right: auto;
+  gap: 6px;
+`;
+
+export const UserName = styled.p`
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 18px;
+  color: #000;
+`;
+
+export const AccountName = styled.p`
+  font-size: 12px;
+  line-height: 15px;
+  color: var(--sub-text-color);
+`;
+
+export const MatchedSpan = styled.span`
+  color: var(--main-color);
+`;
+
+export const UserBannerLink = styled(Link)`
+  text-decoration: none;
 `;
 
 export const TabMenuCont = styled.div`
