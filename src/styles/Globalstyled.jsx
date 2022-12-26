712490dc15f689bd815d1f14b2deee3b41d84926
Jiyeon Ko <goprawgo@gmail.com>
2022-12-26 22:11:53 +0900

    ✨Add : Post 컴포넌트에서 여러장의 사진을 보여주도록 수정

--- src/styles/Globalstyled.jsx
+++ src/styles/Globalstyled.jsx
@@ -1,4 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import reset from 'styled-reset';
 
 const GlobalStyled = createGlobalStyle`
